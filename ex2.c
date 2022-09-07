c3276b57ee0d5f2e760153eadea92514a369d270
nisrine <nisrineelad5@gmail.com>
2022-09-07 16:51:50 +0100

    ex2

---
+++ ex2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   double C, F;
+   printf("C: \n");
+   scanf("%lf", &C);
+   F=(C*1.8)+32;
+   printf("F: %f", F);
+
+    return 0;
+}
---
+++ ex3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   double mile, meter;
+   printf("mile: \n");
+   scanf("%lf", &meter);
+   mile = meter * 1609;
+   printf("the distance in meter is: %lf \n", mile);
+    return 0;
+}
